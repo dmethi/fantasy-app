d6bc4899360345d169c686a80ad6eda7e4dd7a4c
dmethi <37225217+dmethi@users.noreply.github.com>
2022-09-16 17:16:52 -0500

    finished main page chart

--- dataviz.py
+++ dataviz.py
@@ -12,15 +12,14 @@ def homepageTable():
     optimal_points = []
 
     for owner in table_data:
-        names.append(table_data[owner]['team_name'])
-        records.append(str(table_data[owner]['wins']) + ' - ' + str(table_data[owner]['losses']))
+        names.append(table_data[owner]['team_name'] + ' (' + str(table_data[owner]['wins']) + ' - ' + str(table_data[owner]['losses']) + ')')
         power_ranks.append(int(table_data[owner]['power_rank']))
         power_rank_values.append(table_data[owner]['power_rank_value'])
         points_for.append(table_data[owner]['total_points'])
         points_against.append(table_data[owner]['opponent_points'])
         optimal_points.append(table_data[owner]['optimal_points'])
 
-    data = {'Owner': names, 'Record': records, 'Power Rank': power_ranks, 'Power Rank Values': power_rank_values, 'Points For': points_for, 'Points Against': points_against, 'Optimal Points': optimal_points}
+    data = {'Power Rank': power_ranks, 'Owner': names, 'Power Rank Values': power_rank_values, 'Points For': points_for, 'Points Against': points_against, 'Optimal Points': optimal_points}
     th_props = [
         ('font-size', '10px'),
         ('text-align', 'center'),
@@ -33,13 +32,9 @@ def homepageTable():
     df = df.style.hide(axis='index')\
         .format('{:.2f}', subset=['Power Rank Values', 'Points For', 'Points Against', 'Optimal Points'])\
         .set_properties(**{
-            'font-size': '10px',
             'text-align': 'center',
-        }, subset=['Record', 'Power Rank', 'Power Rank Values', 'Points For', 'Points Against', 'Optimal Points'])\
-        .set_properties(**{
-            'font-size': '10px',
-            'text-align': 'left'
-        }, subset=['Owner'])\
+            'height': '30px',
+        })\
         .set_table_styles(styles)\
         .background_gradient(subset=['Power Rank Values', 'Points For', 'Optimal Points'], cmap='RdYlGn')\
         .background_gradient(subset=['Points Against', 'Power Rank'], cmap='RdYlGn_r')
